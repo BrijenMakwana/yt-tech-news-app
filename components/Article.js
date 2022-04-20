5ddc2177b7507fa125471dd6f4f96b9d5e707bf2
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-04-20 10:07:27 +0530

    API fetch

--- components/Article.js
+++ components/Article.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {View,StyleSheet,Text,SafeAreaView,Image} from "react-native";
+import moment from "moment";
 
-const Article = () => {
+const Article = (props) => {
     return(
         <SafeAreaView style={styles.container}>
             {/* image */}
             <Image source={{
-                uri: "https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                uri: props.urlToImage
             }}
             style={styles.image}
             />
@@ -15,19 +16,21 @@ const Article = () => {
 
 
         {/*    title */}
-            <Text style={styles.title}>Tech News Application using React Native</Text>
+            <Text style={styles.title}>{props.title}</Text>
 
         {/*    description */}
-            <Text style={styles.description}>You are watching this tutorial on ProgrammingKnowledge YouTube Channel You are watching this tutorial on ProgrammingKnowledge YouTube Channel</Text>
+            <Text style={styles.description} numberOfLines={3}>
+                {props.description}
+            </Text>
 
             <View style={styles.data}>
-                <Text style={styles.heading}>by: <Text style={styles.author}>Brijen Makwana</Text></Text>
-                <Text style={styles.date}>Aug 9th 22</Text>
+                <Text style={styles.heading}>by: <Text style={styles.author}>{props.author}</Text></Text>
+                <Text style={styles.date}>{moment(props.publishedAt).format("MMM Do YY") }</Text>
             </View>
 
         {/*     source */}
             <View style={{marginTop: 10}}>
-                <Text>source: <Text style={styles.source}>Programmingknowledge</Text></Text>
+                <Text>source: <Text style={styles.source}>{props.sourceName}</Text></Text>
             </View>
             </View>
         </SafeAreaView>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,50 @@
-import React from "react";
-import {View,StyleSheet,Text,SafeAreaView} from "react-native";
+import React, {useEffect, useState} from "react";
+import {View,StyleSheet,Text,SafeAreaView, FlatList} from "react-native";
 import Article from "../components/Article";
+import axios from "axios";
+
 
 const HomeScreen = () => {
+    const [articles,setArticles] = useState([]);
+    const getNews = () => {
+        axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=c1ef3317ba2e48c8aeab23ad33adb6e9',{
+            params:{
+                category: "technology"
+            }
+        })
+            .then( (response) =>{
+                // handle success
+                console.log(response.data.articles);
+                setArticles(response.data.articles);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }
+
+    useEffect(() => {
+        getNews();
+    },[]);
+
     return(
         <SafeAreaView style={styles.container}>
-            <Article/>
+            <FlatList
+                data={articles}
+                renderItem = {({item}) =>
+                    <Article
+                        urlToImage = {item.urlToImage}
+                        title = {item.title}
+                        description = {item.description}
+                        author = {item.author}
+                        publishedAt = {item.publishedAt}
+                        sourceName = {item.source.name}
+                    />}
+                keyExtractor = {(item) => item.title}
+            />
 
         </SafeAreaView>
     )
