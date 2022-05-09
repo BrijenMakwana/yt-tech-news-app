b614ed89a24b4d68b2798d505b281c098c38ff98
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-05-09 11:02:38 +0530

    SearchBar

---
+++ components/SearchBar.js
@@ -0,0 +1,30 @@
+import {View,TextInput,Text,StyleSheet} from "react-native";
+import React from "react";
+
+const SearchBar = (props)=>{
+    return(
+        <View style={styles.container}>
+            <TextInput
+                placeholder="Search"
+                style={styles.input}
+                value={props.searchText}
+                onChangeText={(text)=>props.setSearchText(text)}
+            />
+        </View>
+    )
+}
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+    container:{
+        margin: 10
+    },
+    input:{
+        backgroundColor:"#fff",
+        padding: 10,
+        borderRadius: 10,
+        color: "#000",
+        borderWidth: 1
+    }
+});
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,22 @@
-import react from "react"
-import {View,Text} from "react-native";
+import react, {useState} from "react"
+import {View,Text,StyleSheet} from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+    const [searchText,setSearchText] = useState("");
     return (
-        <View>
-            <Text>Search Screen</Text>
+        <View style={styles.container}>
+            <SearchBar searchText={searchText} setSearchText={setSearchText}/>
+            <Text>{searchText}</Text>
         </View>
     )
 }
 
 export default SearchScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    }
+})
